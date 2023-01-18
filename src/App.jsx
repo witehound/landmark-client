7b1923efcc43673ffe23fbc387d8a49fe1fe7e4c
witehound <netvid00@gmail.com>
2023-01-18 11:26:29 +0100

    turned map to globe

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ function App() {
   return (
     <div>
       <Map
+        container={`map`}
+        projection={`globe`}
         initialViewState={{}}
+        style={{ width: "100vw", height: "100vh" }}
         mapboxAccessToken={import.meta.env.VITE_MAP_BOX_TOKEN}
+        mapStyle={`mapbox://styles/wingedanubis/cld1ff9b7000o01phg2gw97bm`}
       ></Map>
     </div>
   );
