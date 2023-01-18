a46633b1fedb6a49cd63393693344c953eaf4244
witehound <netvid00@gmail.com>
2023-01-18 11:35:31 +0100

    added server proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import "./App.css";
-import Map from "react-map-gl";
+import Map, { NavigationControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
@@ -13,7 +13,9 @@ function App() {
         style={{ width: "100vw", height: "100vh" }}
         mapboxAccessToken={import.meta.env.VITE_MAP_BOX_TOKEN}
         mapStyle={`mapbox://styles/wingedanubis/cld1ff9b7000o01phg2gw97bm`}
-      ></Map>
+      >
+        <NavigationControl />
+      </Map>
     </div>
   );
 }
