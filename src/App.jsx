22e67b3484a632df27e9d76c9d9a4c27af503970
witehound <netvid00@gmail.com>
2023-01-18 09:56:28 +0100

    map component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import * as React from "react";
 import "./App.css";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Map
+        initialViewState={{}}
+        mapboxAccessToken={import.meta.env.VITE_MAP_BOX_TOKEN}
+      ></Map>
+    </div>
+  );
 }
 
 export default App;
